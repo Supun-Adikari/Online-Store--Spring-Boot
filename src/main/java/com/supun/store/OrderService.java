2f0722be794b3c97e265a9d440b33816d31368cf
Supun Adikari <adikariamssh@gmail.com>
2025-04-27 15:40:03 +0530

    Setter INjection added

--- src/main/java/com/supun/store/OrderService.java
+++ src/main/java/com/supun/store/OrderService.java
@@ -4,9 +4,12 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
+  
+
+    public void setPaymentService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
+
     public void placeOrder() {
        
         paymentService.processPayment(100.0);
