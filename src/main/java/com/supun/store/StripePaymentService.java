203cad3e384e0c87488c1eb7c54b32cc7f4b8769
Supun Adikari <adikariamssh@gmail.com>
2025-04-27 11:27:46 +0530

    stripe payment service modified

--- src/main/java/com/supun/store/StripePaymentService.java
+++ src/main/java/com/supun/store/StripePaymentService.java
@@ -1,5 +1,6 @@
 package com.supun.store;
-public class StripePaymentService {
+public class StripePaymentService implements PaymentService {
+    @Override
     public void processPayment(double amount) {
         System.out.println("Stripe");
         System.out.println("Amount: " + amount);
