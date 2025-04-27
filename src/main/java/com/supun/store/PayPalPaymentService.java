ca8103db726af1fba83ba93038f35c657e5ce034
Supun Adikari <adikariamssh@gmail.com>
2025-04-27 15:09:15 +0530

    Paypal service added

---
+++ src/main/java/com/supun/store/PayPalPaymentService.java
@@ -0,0 +1,10 @@
+package com.supun.store;
+
+public class PayPalPaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paypal");
+        System.out.println("Amount: " + amount);
+    }
+    
+}
