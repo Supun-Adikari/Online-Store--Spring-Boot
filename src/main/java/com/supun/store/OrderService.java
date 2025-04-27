78707aa84726f6dd9e707668a97563ad29f3748a
Supun Adikari <adikariamssh@gmail.com>
2025-04-27 15:12:27 +0530

    Contructor injection added

--- src/main/java/com/supun/store/OrderService.java
+++ src/main/java/com/supun/store/OrderService.java
@@ -1,10 +1,14 @@
 package com.supun.store;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
     public void placeOrder() {
        
-        System.out.println("Order placed successfully.");
-        var paymentService = new StripePaymentService();
         paymentService.processPayment(100.0);
     }
 }
