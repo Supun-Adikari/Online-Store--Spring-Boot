2cea17c2f233cda8dceb691b29b6ca4b8d2bc5c8
Supun Adikari <adikariamssh@gmail.com>
2025-04-27 21:34:12 +0530

    Annotaions abd beans added

--- src/main/java/com/supun/store/StripePaymentService.java
+++ src/main/java/com/supun/store/StripePaymentService.java
@@ -1,4 +1,10 @@
 package com.supun.store;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service("stripe")
+@Primary
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
