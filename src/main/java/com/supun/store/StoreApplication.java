37e496380753c0e1ba062eabd230c567d81235d2
Supun Adikari <adikariamssh@gmail.com>
2025-05-03 15:58:15 +0530

    Start changed

--- src/main/java/com/supun/store/StoreApplication.java
+++ src/main/java/com/supun/store/StoreApplication.java
@@ -11,6 +11,7 @@ public class StoreApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=  SpringApplication.run(StoreApplication.class, args);
 		
+		var heavyResource=context.getBean(HeavyResource.class);
 
 		var order=context.getBean(OrderService.class);
 		order.placeOrder();
