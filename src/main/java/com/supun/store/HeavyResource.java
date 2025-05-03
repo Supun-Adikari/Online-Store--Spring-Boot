6c4cccfbc39fdc631b5264da1bf57c17e93b0681
Supun Adikari <adikariamssh@gmail.com>
2025-05-03 15:59:10 +0530

    Heavy resource service added

---
+++ src/main/java/com/supun/store/HeavyResource.java
@@ -0,0 +1,14 @@
+package com.supun.store;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class HeavyResource {
+    public HeavyResource() {
+        System.out.println("HeavyResource initialized");
+    }
+
+    
+}
