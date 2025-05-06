c3aa8fcd7594d56e4c21afef57e558caff75998b
Supun Adikari <adikariamssh@gmail.com>
2025-05-06 12:48:06 +0530

    Order service changed

--- src/main/java/com/supun/store/OrderService.java
+++ src/main/java/com/supun/store/OrderService.java
@@ -3,6 +3,9 @@ package com.supun.store;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 // @Service
 public class OrderService {
 
@@ -10,6 +13,16 @@ public class OrderService {
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
         System.out.println("Order service created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Order service initialized");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Order service destroyed");
     }
 
   
